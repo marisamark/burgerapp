773ec63dfbe1be2a9d81cce09ddba2fbe4d2b354
Marisa Mark <themarisamark@gmail.com>
2019-10-29 09:33:25 -0700

    installed

---
+++ config/config.js
@@ -0,0 +1,25 @@
+exports.configure = function(env) {
+    return configs[env];
+  }
+
+  let configs = {
+    "development": {
+      "username": "root",
+      "password": `${process.env.DB_PASSWORD}`,
+      "database": "burger_db",
+      "host": "127.0.0.1",
+      "port": 3306,
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "port": 3306,
+      "dialect": "mysql"
+    },
+    "production": {
+        "use_env_variable":"JAWSDB_URL"
+    }
+  }
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("everything", 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("monster", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("california", 1);
