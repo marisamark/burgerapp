92a520d2c4b83412e68fc019e01cc27b6fe3fcad
Marisa Mark <themarisamark@gmail.com>
2019-10-31 10:04:24 -0700

    burgers can now be devoured

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,13 +7,13 @@ var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 // get route -> index
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req, res) {
-    // render index using burger data
-  burger.all(function(data) {
+router.get("/burgers", function (req, res) {
+  // render index using burger data
+  burger.all(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -22,28 +22,51 @@ router.get("/burgers", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-    // save new burger and redirect to '/'
-    console.log('body',req.body)
+router.post("/burgers/create", function (req, res) {
+  const { burger_name } = req.body;
+  burger.create(['burger_name', 'devoured'], [burger_name, 0], function (result) {
+    console.log(result);
+    res.redirect("/burgers")
+  })
+})
+
+router.post('/burgers/update', function (req, res) {
+  const { id, devoured } = req.body;
+  var condition = "id = " + id;
+  burger.update({
+    devoured,
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.redirect("/burgers")
+    }
+  });
+})
+
+router.post("/api/burgers", function (req, res) {
+  // save new burger and redirect to '/'
+  console.log('body', req.body)
   burger.create([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, parseInt(req.body.devoured)
-  ], function(result) {
+  ], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/burgers/:id", function(req, res) {
-    // save modified burger and redirect to '/'
+router.put("/api/burgers/:id", function (req, res) {
+  // save modified burger and redirect to '/'
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     devoured: true,
-  }, condition, function(result) {
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -53,10 +76,10 @@ router.put("/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/burgers/:id", function(req, res) {
+router.delete("/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
+  burger.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -68,3 +91,5 @@ router.delete("/burgers/:id", function(req, res) {
 
 // Export routes for server.js to use.
 module.exports = router;
+
+
