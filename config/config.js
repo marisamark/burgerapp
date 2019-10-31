a55f70fa619c69e0d98a9fcb11ecb77b20c44d60
Marisa Mark <themarisamark@gmail.com>
2019-10-31 12:43:20 -0700

    adding new heroku info

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ exports.configure = function(env) {
 
   let configs = {
     "development": {
-      "username": "root",
+      "username": "v2osuf93esb1pxmw",
       "password": `${process.env.DB_PASSWORD}`,
-      "database": "burger_db",
-      "host": "127.0.0.1",
+      "database": "ghg3kbsi33d69x4o",
+      "host": "op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       "port": 3306,
       "dialect": "mysql"
     },
